3238bc6db000bcd01f2fb94948f31e3021f4dc00
IsmaelBJ07 <ibeljim939@g.educaand.es>
2026-03-13 21:16:20 +0100

    a

--- Ejercicios/SQL/Relación ejercicios 03 UD 04 - Subconsultas -.sql
+++ Ejercicios/SQL/Relación ejercicios 03 UD 04 - Subconsultas -.sql
@@ -2,190 +2,240 @@
 SELECT jugador.codigo, jugador.nombre, jugador.procedencia, jugador.altura, jugador.peso
 FROM jugador
 WHERE jugador.altura > (
-	SELECT AVG(altura)
-	FROM jugador
+    SELECT AVG(altura)
+    FROM jugador
 );
 
 --Ejercicio02
-SELECT DISTINCT jugador.nombre AS jugador, equipo.nombre AS equipo, jugador_equipo.temporada
+SELECT DISTINCT jugador.nombre, equipo.nombre, jugador_equipo.temporada
 FROM jugador
 INNER JOIN jugador_equipo ON jugador.codigo = jugador_equipo.codigo_jugador
 INNER JOIN equipo ON equipo.codigo = jugador_equipo.codigo_equipo
 WHERE jugador_equipo.temporada = '2020-21'
-	AND equipo.nombre != 'Raptors'
-	AND (
-		equipo.codigo IN (SELECT equipo_local FROM partido WHERE equipo_visitante = (SELECT codigo FROM equipo WHERE nombre = 'Raptors') AND temporada = '2020-21')
-		OR
-		equipo.codigo IN (SELECT equipo_visitante FROM partido WHERE equipo_local = (SELECT codigo FROM equipo WHERE nombre = 'Raptors') AND temporada = '2020-21')
-	);
+AND equipo.nombre != 'Raptors'
+AND (
+    equipo.codigo IN (
+        SELECT equipo_local
+        FROM partido
+        WHERE equipo_visitante = (
+            SELECT codigo
+            FROM equipo
+            WHERE nombre = 'Raptors'
+        )
+        AND temporada = '2020-21'
+    )
+    OR
+    equipo.codigo IN (
+        SELECT equipo_visitante
+        FROM partido
+        WHERE equipo_local = (
+            SELECT codigo
+            FROM equipo
+            WHERE nombre = 'Raptors'
+        )
+        AND temporada = '2020-21'
+    )
+);
 
 --Ejercicio03
 SELECT jugador.nombre
 FROM jugador
 WHERE jugador.codigo NOT IN (
-	SELECT id_jugador
-	FROM estadistica_jugador_temporada
-	WHERE temporada = '2018-19' AND promedio_puntos_partido_temporada > 15
+    SELECT id_jugador
+    FROM estadistica_jugador_temporada
+    WHERE temporada = '2018-19'
+    AND promedio_puntos_partido_temporada > 15
 )
 AND jugador.codigo IN (
-	SELECT id_jugador
-	FROM estadistica_jugador_temporada
-	WHERE temporada = '2018-19'
+    SELECT id_jugador
+    FROM estadistica_jugador_temporada
+    WHERE temporada = '2018-19'
 );
 
 --Ejercicio04
 SELECT DISTINCT jugador.nombre
 FROM jugador
-INNER JOIN estadistica_jugador_temporada ejt ON jugador.codigo = ejt.id_jugador
-INNER JOIN equipo ON equipo.codigo = ejt.id_equipo
+INNER JOIN estadistica_jugador_temporada ON jugador.codigo = estadistica_jugador_temporada.id_jugador
+INNER JOIN equipo ON equipo.codigo = estadistica_jugador_temporada.id_equipo
 WHERE equipo.conferencia = 'Este'
-	AND ejt.temporada = '2017-18'
-	AND ejt.promedio_puntos_partido_temporada > 700;
+AND estadistica_jugador_temporada.temporada = '2017-18'
+AND estadistica_jugador_temporada.promedio_puntos_partido_temporada > 700
+ORDER BY jugador.nombre;
 
 --Ejercicio05
 SELECT jugador.nombre
 FROM jugador
-INNER JOIN estadistica_jugador_temporada ejt ON jugador.codigo = ejt.id_jugador
-WHERE ejt.temporada = '2012-13'
-	AND ejt.promedio_tapones_temporada > 80;
+INNER JOIN estadistica_jugador_temporada ON jugador.codigo = estadistica_jugador_temporada.id_jugador
+WHERE estadistica_jugador_temporada.temporada = '2012-13'
+AND estadistica_jugador_temporada.promedio_tapones_temporada > 80;
 
 --Ejercicio06
-SELECT jugador.nombre AS jugador, jugador.altura
+SELECT jugador.nombre, jugador.altura
 FROM jugador
 WHERE jugador.altura > (
-	SELECT AVG(j.altura)
-	FROM jugador j
-	INNER JOIN jugador_equipo je ON j.codigo = je.codigo_jugador
-	INNER JOIN equipo e ON e.codigo = je.codigo_equipo
-	WHERE e.nombre = 'Timberwolves' AND je.temporada = '2021-22'
+    SELECT AVG(jugador.altura)
+    FROM jugador
+    INNER JOIN jugador_equipo ON jugador.codigo = jugador_equipo.codigo_jugador
+    INNER JOIN equipo ON equipo.codigo = jugador_equipo.codigo_equipo
+    WHERE equipo.nombre = 'Timberwolves'
+    AND jugador_equipo.temporada = '2021-22'
 );
 
 --Ejercicio07
-SELECT DISTINCT equipo.nombre AS Nombre_Equipo
+SELECT DISTINCT equipo.nombre
 FROM equipo
-INNER JOIN jugador_equipo je ON equipo.codigo = je.codigo_equipo
-INNER JOIN jugador j ON j.codigo = je.codigo_jugador
+INNER JOIN jugador_equipo ON equipo.codigo = jugador_equipo.codigo_equipo
+INNER JOIN jugador ON jugador.codigo = jugador_equipo.codigo_jugador
 WHERE equipo.nombre LIKE 'C%'
-	AND je.temporada = '2024-25'
-	AND j.peso > 100;
+AND jugador_equipo.temporada = '2024-25'
+AND jugador.peso > 100;
 
 --Ejercicio08
-SELECT j.nombre AS Nombre_Jugador, ejt.temporada, ejt.promedio_puntos_partido_temporada
-FROM jugador j
-INNER JOIN estadistica_jugador_temporada ejt ON j.codigo = ejt.id_jugador
-WHERE ejt.temporada = '2017-18'
-	AND ejt.promedio_puntos_partido_temporada > (
-		SELECT AVG(promedio_puntos_partido_temporada)
-		FROM estadistica_jugador_temporada
-		WHERE temporada = '2017-18'
-	);
+SELECT jugador.nombre, estadistica_jugador_temporada.temporada, estadistica_jugador_temporada.promedio_puntos_partido_temporada
+FROM jugador
+INNER JOIN estadistica_jugador_temporada ON jugador.codigo = estadistica_jugador_temporada.id_jugador
+WHERE estadistica_jugador_temporada.temporada = '2017-18'
+AND estadistica_jugador_temporada.promedio_puntos_partido_temporada > (
+    SELECT AVG(promedio_puntos_partido_temporada)
+    FROM estadistica_jugador_temporada
+    WHERE temporada = '2017-18'
+);
 
 --Ejercicio09
-SELECT ejt1.temporada, j.nombre AS nombre_jugador, e.nombre AS nombre_equipo, ejt1.promedio_tiros_libres_temporada
-FROM estadistica_jugador_temporada ejt1
-INNER JOIN jugador j ON j.codigo = ejt1.id_jugador
-INNER JOIN equipo e ON e.codigo = ejt1.id_equipo
-WHERE ejt1.promedio_tiros_libres_temporada = (
-	SELECT MAX(promedio_tiros_libres_temporada)
-	FROM estadistica_jugador_temporada ejt2
-	WHERE ejt2.temporada = ejt1.temporada
+SELECT estadistica_jugador_temporada.temporada, jugador.nombre, equipo.nombre, estadistica_jugador_temporada.promedio_tiros_libres_temporada
+FROM estadistica_jugador_temporada
+INNER JOIN jugador ON jugador.codigo = estadistica_jugador_temporada.id_jugador
+INNER JOIN equipo ON equipo.codigo = estadistica_jugador_temporada.id_equipo
+WHERE estadistica_jugador_temporada.promedio_tiros_libres_temporada = (
+    SELECT MAX(promedio_tiros_libres_temporada)
+    FROM estadistica_jugador_temporada estadistica2
+    WHERE estadistica2.temporada = estadistica_jugador_temporada.temporada
 );
 
 --Ejercicio10
-SELECT j.nombre AS jugador, e.nombre AS equipo
-FROM jugador j
-INNER JOIN estadistica_jugador_temporada ejt ON j.codigo = ejt.id_jugador
-INNER JOIN equipo e ON e.codigo = ejt.id_equipo
-WHERE ejt.temporada = '2019-20'
-	AND ejt.total_partidos_jugados > 36;
+SELECT jugador.nombre, equipo.nombre
+FROM jugador
+INNER JOIN estadistica_jugador_temporada ON jugador.codigo = estadistica_jugador_temporada.id_jugador
+INNER JOIN equipo ON equipo.codigo = estadistica_jugador_temporada.id_equipo
+WHERE estadistica_jugador_temporada.temporada = '2019-20'
+AND estadistica_jugador_temporada.total_partidos_jugados > 36;
 
 --Ejercicio11
-SELECT j.nombre AS jugador, e.nombre AS equipo
-FROM jugador j
-INNER JOIN estadistica_jugador_temporada ejt ON j.codigo = ejt.id_jugador
-INNER JOIN equipo e ON e.codigo = ejt.id_equipo
-WHERE ejt.temporada = '2020-21'
-	AND ejt.promedio_asistencia_temporadas < 3;
+SELECT jugador.nombre, equipo.nombre
+FROM jugador
+INNER JOIN estadistica_jugador_temporada ON jugador.codigo = estadistica_jugador_temporada.id_jugador
+INNER JOIN equipo ON equipo.codigo = estadistica_jugador_temporada.id_equipo
+WHERE estadistica_jugador_temporada.temporada = '2020-21'
+AND estadistica_jugador_temporada.promedio_asistencia_temporadas < 3;
 
 --Ejercicio12
-SELECT j.nombre AS jugador, e.nombre AS equipo
-FROM jugador j
-INNER JOIN estadistica_jugador_temporada ejt ON j.codigo = ejt.id_jugador
-INNER JOIN equipo e ON e.codigo = ejt.id_equipo
-WHERE ejt.temporada = '2021-22'
-	AND ejt.promedio_puntos_partido_temporada > (
-		SELECT AVG(promedio_puntos_partido_temporada)
-		FROM estadistica_jugador_temporada
-		WHERE temporada = '2021-22'
-	);
+SELECT jugador.nombre, equipo.nombre
+FROM jugador
+INNER JOIN estadistica_jugador_temporada ON jugador.codigo = estadistica_jugador_temporada.id_jugador
+INNER JOIN equipo ON equipo.codigo = estadistica_jugador_temporada.id_equipo
+WHERE estadistica_jugador_temporada.temporada = '2021-22'
+AND estadistica_jugador_temporada.promedio_puntos_partido_temporada > (
+    SELECT AVG(promedio_puntos_partido_temporada)
+    FROM estadistica_jugador_temporada
+    WHERE temporada = '2021-22'
+);
 
 --Ejercicio13
-SELECT DISTINCT j.nombre
-FROM jugador j
-INNER JOIN estadistica_jugador_temporada ejt ON j.codigo = ejt.id_jugador
-INNER JOIN equipo e ON e.codigo = ejt.id_equipo
-WHERE ejt.temporada = '2023-24'
-	AND e.conferencia = 'Este'
-	AND ejt.promedio_puntos_partido_temporada >= 375
-	AND e.codigo IN (
-		SELECT equipo_visitante
-		FROM partido
-		WHERE temporada = '2023-24'
-	);
+SELECT DISTINCT jugador.nombre
+FROM jugador
+INNER JOIN estadistica_jugador_temporada ON jugador.codigo = estadistica_jugador_temporada.id_jugador
+INNER JOIN equipo ON equipo.codigo = estadistica_jugador_temporada.id_equipo
+WHERE estadistica_jugador_temporada.temporada = '2023-24'
+AND equipo.conferencia = 'Este'
+AND estadistica_jugador_temporada.promedio_puntos_partido_temporada >= 375
+AND equipo.codigo IN (
+    SELECT equipo_visitante
+    FROM partido
+    WHERE temporada = '2023-24'
+);
 
 --Ejercicio14
-SELECT j.codigo AS codigo_jugador, j.nombre AS nombre_jugador, e.nombre AS nombre_equipo, ejt.promedio_robos_temporada
-FROM jugador j
-INNER JOIN estadistica_jugador_temporada ejt ON j.codigo = ejt.id_jugador
-INNER JOIN equipo e ON e.codigo = ejt.id_equipo
-WHERE ejt.temporada = '2023-24'
-	AND ejt.promedio_robos_temporada > (
-		SELECT AVG(promedio_robos_temporada)
-		FROM estadistica_jugador_temporada ejt2
-		INNER JOIN equipo e2 ON e2.codigo = ejt2.id_equipo
-		WHERE ejt2.temporada = '2023-24' AND e2.nombre = 'Heat'
-	);
+SELECT jugador.codigo, jugador.nombre, equipo.nombre, estadistica_jugador_temporada.promedio_robos_temporada
+FROM jugador
+INNER JOIN estadistica_jugador_temporada ON jugador.codigo = estadistica_jugador_temporada.id_jugador
+INNER JOIN equipo ON equipo.codigo = estadistica_jugador_temporada.id_equipo
+WHERE estadistica_jugador_temporada.temporada = '2023-24'
+AND estadistica_jugador_temporada.promedio_robos_temporada > (
+    SELECT AVG(estadistica_jugador_temporada.promedio_robos_temporada)
+    FROM estadistica_jugador_temporada
+    INNER JOIN equipo ON equipo.codigo = estadistica_jugador_temporada.id_equipo
+    WHERE estadistica_jugador_temporada.temporada = '2023-24'
+    AND equipo.nombre = 'Heat'
+);
 
 --Ejercicio15
-SELECT jugador.nombre AS nombre, equipo.nombre AS equipo, ejt_cp.temporada 
-FROM estadistica_jugador_temporada ejt_cp
-INNER JOIN jugador_equipo ON jugador_equipo.codigo_jugador = ejt_cp.id_jugador 
-	AND jugador_equipo.codigo_equipo = ejt_cp.id_equipo 
-	AND jugador_equipo.temporada = ejt_cp.temporada
+SELECT jugador.nombre, equipo.nombre, estadistica_jugador_temporada.temporada
+FROM estadistica_jugador_temporada
+INNER JOIN jugador_equipo ON jugador_equipo.codigo_jugador = estadistica_jugador_temporada.id_jugador 
+AND jugador_equipo.codigo_equipo = estadistica_jugador_temporada.id_equipo 
+AND jugador_equipo.temporada = estadistica_jugador_temporada.temporada
 INNER JOIN jugador ON jugador.codigo = jugador_equipo.codigo_jugador
 INNER JOIN equipo ON equipo.codigo = jugador_equipo.codigo_equipo
-WHERE ejt_cp.promedio_puntos_partido_temporada >
-	(
-	SELECT AVG(promedio_puntos_partido_temporada)
-	FROM estadistica_jugador_temporada
-	WHERE estadistica_jugador_temporada.temporada = ejt_cp.temporada
-		AND estadistica_jugador_temporada.id_equipo = ejt_cp.id_equipo
-	)
+WHERE estadistica_jugador_temporada.promedio_puntos_partido_temporada > (
+    SELECT AVG(promedio_puntos_partido_temporada)
+    FROM estadistica_jugador_temporada estadistica2
+    WHERE estadistica2.temporada = estadistica_jugador_temporada.temporada
+    AND estadistica2.id_equipo = estadistica_jugador_temporada.id_equipo
+)
 ORDER BY jugador.nombre, equipo.nombre;
 
 --Ejercicio16
-SELECT e.nombre,
-	(SELECT COUNT(*) FROM partido p1 WHERE p1.equipo_local = e.codigo AND p1.temporada = '2023-24' AND p1.puntos_local > p1.puntos_visitante) AS partidos_ganados_como_local,
-	(SELECT COUNT(*) FROM partido p2 WHERE p2.equipo_visitante = e.codigo AND p2.temporada = '2023-24' AND p2.puntos_visitante > p2.puntos_local) AS partidos_ganados_como_visitante
-FROM equipo e
-WHERE (SELECT COUNT(*) FROM partido p1 WHERE p1.equipo_local = e.codigo AND p1.temporada = '2023-24' AND p1.puntos_local > p1.puntos_visitante) > 
-	  (SELECT COUNT(*) FROM partido p2 WHERE p2.equipo_visitante = e.codigo AND p2.temporada = '2023-24' AND p2.puntos_visitante > p2.puntos_local)
-ORDER BY e.nombre;
+SELECT equipo.nombre,
+(
+    SELECT COUNT(*)
+    FROM partido
+    WHERE partido.equipo_local = equipo.codigo
+    AND partido.temporada = '2023-24'
+    AND partido.puntos_local > partido.puntos_visitante
+) AS partidos_ganados_como_local,
+(
+    SELECT COUNT(*)
+    FROM partido
+    WHERE partido.equipo_visitante = equipo.codigo
+    AND partido.temporada = '2023-24'
+    AND partido.puntos_visitante > partido.puntos_local
+) AS partidos_ganados_como_visitante
+FROM equipo
+WHERE
+(
+    SELECT COUNT(*)
+    FROM partido
+    WHERE partido.equipo_local = equipo.codigo
+    AND partido.temporada = '2023-24'
+    AND partido.puntos_local > partido.puntos_visitante
+)
+>
+(
+    SELECT COUNT(*)
+    FROM partido
+    WHERE partido.equipo_visitante = equipo.codigo
+    AND partido.temporada = '2023-24'
+    AND partido.puntos_visitante > partido.puntos_local
+) 
+ORDER BY equipo.nombre;
 
 --Ejercicio17
-SELECT e.nombre, p_local.temporada,
-	AVG(p_local.porcentaje_triples_anotados_local) AS media_triples_local,
-	(SELECT AVG(p_visitante.porcentaje_triples_anotados_visitante) 
-	 FROM partido p_visitante 
-	 WHERE p_visitante.equipo_visitante = e.codigo 
-	 AND p_visitante.temporada = p_local.temporada) AS media_triples_visitante
-FROM equipo e
-INNER JOIN partido p_local ON e.codigo = p_local.equipo_local
-GROUP BY e.codigo, e.nombre, p_local.temporada
-HAVING AVG(p_local.porcentaje_triples_anotados_local) > (
-	 SELECT AVG(p_visitante.porcentaje_triples_anotados_visitante) 
-	 FROM partido p_visitante 
-	 WHERE p_visitante.equipo_visitante = e.codigo 
-	 AND p_visitante.temporada = p_local.temporada
+SELECT equipo.nombre, partido.temporada,
+AVG(partido.porcentaje_triples_anotados_local) AS media_triples_local,
+(
+    SELECT AVG(partido_visitante.porcentaje_triples_anotados_visitante) 
+    FROM partido partido_visitante
+    WHERE partido_visitante.equipo_visitante = equipo.codigo
+    AND partido_visitante.temporada = partido.temporada
+) AS media_triples_visitantes
+FROM equipo
+INNER JOIN partido ON equipo.codigo = partido.equipo_local
+GROUP BY equipo.codigo, equipo.nombre, partido.temporada
+HAVING AVG(partido.porcentaje_triples_anotados_local) >
+(
+    SELECT AVG(partido_visitante.porcentaje_triples_anotados_visitante)
+    FROM partido partido_visitante
+    WHERE partido_visitante.equipo_visitante = equipo.codigo
+    AND partido_visitante.temporada = partido.temporada
 )
-ORDER BY e.nombre, p_local.temporada;
+ORDER BY equipo.nombre, partido.temporada;
